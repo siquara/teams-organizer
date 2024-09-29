c40db6329cbe381711dead6e3750bd037084e4fa
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-29 13:08:34 -0300

    Feat [ Configuracao do tema e StatusBar]

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { Groups } from "@screens/Groups";
-
+import theme from "@theme/index";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor= "transparent"
+        translucent  
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
-
-

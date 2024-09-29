c6f15caca9d97271eb546f3e4bbf3d3fe3762eaa
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-29 17:43:04 -0300

    Feat [Criacao do ListEmpty e configuracao do FlatList]

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,31 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState([
+    "Galera teste",
+    "Amigos do Futebol",
+    "Família",
+    "Trabalho",
+  ]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua Turma" />
-      <GroupCard title="Povo do Churras"  />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhuma turma encontrada" />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+      />
     </Container>
   );
 }

c1320a7b258d3078b37eaaa6d3d71577355baa66
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-29 17:22:32 -0300

    Feat [Componente do GroupCard]

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua Turma" />
+      <GroupCard title="Povo do Churras"  />
     </Container>
   );
 }

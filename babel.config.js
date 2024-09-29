ee0a5e0c67b12bca4a9cc2e61608dd42838bed3a
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-29 11:42:46 -0300

    Feat [Interface da aplicação]

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@assets": "./src/assets",
+            "@utils": "./src/utils",
+            "@storage": "./src/storage",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
   };
 };

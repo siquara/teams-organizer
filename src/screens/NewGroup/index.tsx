f73ad72c82e4204b5fb4d529b08be29c88b9bbc7
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-10-05 16:48:23 -0300

    Finalizando navegacao

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,11 +4,13 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
   const navigation = useNavigation();
   function handleNew() {
-    navigation.navigate("players", { group: "SiqTeam" });
+    navigation.navigate("players", { group });
   }
   return (
     <Container>
@@ -19,7 +21,7 @@ export function NewGroup() {
           title="Nova Turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
         <Button
           title="Criar Turma"
           style={{ marginTop: 20 }}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,7 +9,11 @@ import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useRoute } from "@react-navigation/native";
 
+type RouteParams = {
+    group: string;
+}
 export function Players() {
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([
@@ -23,11 +27,14 @@ export function Players() {
     "Oiiii",
     "Manda mais",
   ]);
+
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
   return (
     <Container>
       <Header showBackButton />
       <Highlight
-        title="Nome da Turma "
+        title={group}
         subtitle="Adicione a galera e separe os times"
       />
       <Form>
@@ -67,7 +74,7 @@ export function Players() {
           players.length === 0 && { flex: 1 },
         ]}
       />
-      <Button title="Remover Turma"type="secondary" />
+      <Button title="Remover Turma" type="secondary" />
     </Container>
   );
 }
